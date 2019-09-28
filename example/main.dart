dda4d347349d4e013e8b6c84c1d44f088cea9a91
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-09-28 17:55:21 -0300

    update example

--- example/main.dart
+++ example/main.dart
@@ -1,8 +1,11 @@
 import 'package:kana/kana.dart';
 
-void main() {  
-  print("Hiragana ${getHiragana("a")} from Romaji a");
-  print("Katakana ${getKatakana("a")} from Romaji a");
-  print("Romaji ${getRomajiFromHiragana("あ")} from Hiragana あ");
-  print("Romaji ${getRomajiFromKatakana("ア")} from Katakana ア");
+void main() {
+  final r = "shi";
+  final h = "ゆ";
+  final k = "ア";
+  print("Hiragana ${getHiragana(r)} from Romaji $r");
+  print("Katakana ${getKatakana(r)} from Romaji $r");
+  print("Romaji ${getRomajiFromHiragana(h)} from Hiragana $h");
+  print("Romaji ${getRomajiFromKatakana(k)} from Katakana $k");
 }
