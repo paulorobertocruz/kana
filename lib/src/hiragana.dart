842b87dd833b12d4825b9533592e6a02e54bc6e4
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-09-28 16:40:39 -0300

    add return statements

--- lib/src/hiragana.dart
+++ lib/src/hiragana.dart
@@ -2,8 +2,10 @@ import 'package:kana/src/data.dart' show ROMAJI_HIRAGANA, HIRAGANA_ROMAJI;
 
 String getHiragana(String romaji) {
   if (ROMAJI_HIRAGANA.containsKey(romaji)) return ROMAJI_HIRAGANA[romaji];
+  return null;
 }
 
 String getRomajiFromHiragana(String hiragana) {
   if (HIRAGANA_ROMAJI.containsKey(hiragana)) return HIRAGANA_ROMAJI[hiragana];
+  return null;
 }
--- lib/src/katakana.dart
+++ lib/src/katakana.dart
@@ -2,8 +2,10 @@ import 'package:kana/src/data.dart' show ROMAJI_KATAKANA, KATAKANA_ROMAJI;
 
 String getKatakana(String romaji) {
   if (ROMAJI_KATAKANA.containsKey(romaji)) return ROMAJI_KATAKANA[romaji];
+  return null;
 }
 
 String getRomajiFromKatakana(String hiragana) {
   if (KATAKANA_ROMAJI.containsKey(hiragana)) return KATAKANA_ROMAJI[hiragana];
+  return null;
 }
