cd42418bcd56a1977a9b8e72439da4719433d8fc
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-12-19 20:37:56 -0300

    isCharConsonant for lower and upper case chars

--- lib/src/utils/is_char_consonant.dart
+++ lib/src/utils/is_char_consonant.dart
@@ -2,7 +2,8 @@ import 'package:kana/src/utils/is_empty.dart';
 
 bool isCharConsonant(String char, [bool includeY = true]) {
   if (isEmpty(char)) return false;
+  if (char.length > 1) return false;
   final regexp = RegExp(
-      includeY ? r"/[bcdfghjklmnpqrstvwxyz]/" : r"/[bcdfghjklmnpqrstvwxz]/");
-  return regexp.hasMatch(char[0]);
+      includeY ? r"[bcdfghjklmnpqrstvwxyz]" : r"[bcdfghjklmnpqrstvwxz]");
+  return regexp.hasMatch(char[0].toLowerCase());
 }
