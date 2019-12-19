93b67ee0dbb61953436af7fcc31c07cf3436c2d5
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-12-19 20:33:49 -0300

    remove 1.0 api

--- lib/kana.dart
+++ lib/kana.dart
@@ -1,9 +1,6 @@
 export 'package:kana/src/utils/utils.dart';
 export 'package:kana/src/constants.dart';
 export 'package:kana/src/to_hiragana.dart';
-export 'package:kana/src/to_kana.dart' hide customMapping;
-export 'package:kana/src/to_romaji.dart' hide customMapping;
-
-export 'package:kana/src/katakana.dart';
-export 'package:kana/src/hiragana.dart';
+export 'package:kana/src/to_kana.dart' show toKana;
+export 'package:kana/src/to_romaji.dart' show toRomaji;
 export 'package:kana/src/data.dart';
